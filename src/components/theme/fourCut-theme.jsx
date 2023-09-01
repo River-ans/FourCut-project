d2b741ab3c5c1025fcf24a04455d825e40ce025b
River-ans <mriveroom@gmail.com>
2023-09-01 19:34:07 +0900

    이미지 로드에 대한 처리 그리고 readme 수정

--- src/components/theme/fourCut-theme.jsx
+++ src/components/theme/fourCut-theme.jsx
@@ -1,14 +1,36 @@
 import style from "../../styles/theme.module.scss";
+import React, { useState, useEffect } from "react";
 
 const FourCutTheme = ({ imgs, theme = "pink" }) => {
   const containerClasses = [style.imgContainer, style[theme]];
+
+  const [loadedImages, setLoadedImages] = useState(0);
+  const [allImagesLoaded, setAllImagesLoaded] = useState(false);
+
+  useEffect(() => {
+    if (imgs && imgs.length > 0 && loadedImages === imgs.length) {
+      setAllImagesLoaded(true);
+    }
+  }, [loadedImages, imgs]);
+
+  const handleImageLoad = () => {
+    if (imgs && imgs.length > 0) {
+      setLoadedImages((prev) => prev + 1);
+    }
+    return allImagesLoaded;
+  };
 
   return (
     <div className={containerClasses.join(" ")}>
       {imgs && imgs.length > 0 ? (
         imgs.map((image, index) => (
           <div key={index} className={style.imgBox}>
-            <img src={image.imgUrl} alt={`image-${image.id}`} />
+            <img
+              src={image.imgUrl}
+              alt={`image-${image.id}`}
+              onLoad={handleImageLoad}
+            />
+            {allImagesLoaded ? <></> : <div className={style.imgLoading}></div>}
           </div>
         ))
       ) : (
